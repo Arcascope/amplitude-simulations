4172b928754e610b77ed5f81686160229c95a839
ojwalch <ojwalch@gmail.com>
2024-10-30 15:30:46 -0400

    Final tweaks

--- make_all_figures.py
+++ make_all_figures.py
@@ -1,7 +1,7 @@
 import subprocess
 
 if __name__ == '__main__':
-
+    # You will also need to run the Jupyter notebook amplitude_boost_plots.ipynb if you haven't already
     file_paths = ["assemble_response_curve_figure.py",
                   "make_sleep_irregularity_figure.py",
                   "make_melatonin_figure.py"]
